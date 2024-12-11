2a07f4bb2adbb99715ddf54d58f9f4c060086a1b
Micke Öhlén <tech@igpholding.com>
2024-12-11 14:19:30 +0100

    Implemented improved cookie consent with proper GTM integration

--- .output/public/_nuxt/CmQSWefi.js
+++
@@ -1,1 +0,0 @@
-import{aj as a,ak as o}from"./AuDPTbWI.js";const s=a((t,r)=>{const e=o("lang",e)});export{s as default};
---
+++ .output/public/_nuxt/fpjigZhT.js
@@ -0,0 +1,1 @@
+import{aj as a,ak as o}from"./LRQc7Xda.js";const s=a((t,r)=>{const e=o("lang",e)});export{s as default};
